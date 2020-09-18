b0936eb88dc1d2b2ea5095ae4cc3377e09f6ac45
Jay Ess <33491863+Spearfield@users.noreply.github.com>
2020-09-18 23:19:26 +0200

    Cleanup.

--- PoolTest/Ball.gd
+++ PoolTest/Ball.gd
@@ -1,20 +1,10 @@
 extends RigidBody2D
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("click"):
 		var angle : float = get_angle_to(get_global_mouse_position())
 		var direction : Vector2 = Vector2( cos(angle), sin(angle) )
-		apply_impulse(Vector2(),direction * 100)
+		apply_impulse(Vector2(),direction * 500)
 	pass
